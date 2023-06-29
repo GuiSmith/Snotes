7163523da6de5672668f6ba34285815e183b9a18
Guilherme <guilhermessmith2014@gmail.com>
2023-06-29 13:36:52 -0300

    Profile updated

--- project/main.php
+++ project/main.php
@@ -22,6 +22,7 @@
 		<link rel="stylesheet" type="text/css" href = "navbar/navbar.css">
 
 		<!-- Page Style -->
+		<link rel="stylesheet" type="text/css" href = "index.css">
 
 		<!-- Bootstrap -->
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
@@ -35,47 +36,51 @@
 	
 	<body>
 
-		<?php
+		<?php require "navbar/navbar.php" ?>
 
-			require "navbar/navbar.php";
+		<section>
+			
+			<?php
 
-			if (isset($_GET["page"])) {
+				if (isset($_GET["page"])) {
 
-				for ($i=0; $i < count($objects); $i++) {
-					
-					if ($_GET["page"] == $objects[$i]->pageCode) {
+					for ($i=0; $i < count($objects); $i++) {
 						
-						require $objects[$i]->fileName . "/index.php";
+						if ($_GET["page"] == $objects[$i]->pageCode) {
+							
+							require $objects[$i]->fileName . "/index.php";
 
-						$page_title = $objects[$i]->displayName;
+							$page_title = $objects[$i]->displayName;
 
-						break;
+							break;
 
-					}
-
-				}
-				
-			}else{
-
-				if (isset($_GET["passcode"])) {
-
-					$page_passcode = $_GET["passcode"];
-
-					$page_passcode_sql = "SELECT passcode FROM users WHERE passcode = '$page_passcode'";
-
-					$page_passcode_result = mysqli_query($conn, $page_passcode_sql);
-
-					if (mysqli_num_rows($page_passcode_result) > 0) {
-						
-						require "password/new_password.php";
+						}
 
 					}
 					
+				}else{
+
+					if (isset($_GET["passcode"])) {
+
+						$page_passcode = $_GET["passcode"];
+
+						$page_passcode_sql = "SELECT passcode FROM users WHERE passcode = '$page_passcode'";
+
+						$page_passcode_result = mysqli_query($conn, $page_passcode_sql);
+
+						if (mysqli_num_rows($page_passcode_result) > 0) {
+							
+							require "password/new_password.php";
+
+						}
+						
+					}
+
 				}
 
-			}
+			?>
 
-		?>
+		</section>
 
 		<script>
 
--- project/profile/index.php
+++ project/profile/index.php
@@ -1,3 +1,21 @@
+<div class = "row" >
+	
+	<div class = "col-sm-6" >
+		
+		<h2 class = "text-center" >Perfil</h2>
+
+	</div>
+
+	<div class = "col-sm-6" >
+		
+		<h2 class = "text-center" >Configurações</h2>
+
+	</div>
+
+</div>
+
+<!-- página de exemplo teste -->
+
 <div>
 	
 	<h2>
@@ -22,8 +40,26 @@
 
 		</li>
 
-		TEsting a lot
+		<li>
+			
+			Criação: <?php echo $_SESSION["user"]["created_at"] ?>
+
+		</li>
+
+		<li>
+			
+			Atualização: <?php echo $_SESSION["user"]["updated_at"] ?>
+
+		</li>
+
+		<li>
+			
+			Login: <?php echo $_SESSION["user"]["logged_at"] ?>
+
+		</li>
 
 	</ul>
 
 </div>
+
+<?php //foreach ($_SESSION["user"] as $key => $value) echo $key . ": " . $value . "<br>"; ?>
